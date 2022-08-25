3749ef0f8be082603488184f0391a910852829d0
falcaomm <manu.m.falcao@gmail.com>
2022-08-25 16:18:14 -0300

    ex 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,15 @@
 const arrayNumeros = [0, 1, 2, 3, 4, 5, 6, 7]
 
 const multiplica3 = arrayNumeros.map((item, indice, array) => {
-    triplos = item * 3
-    return triplos
+    arrayTriplos = item * 3
+    return arrayTriplos
 })
 
 console.log(multiplica3);
 
 const divide2 = arrayNumeros.map((item, indice, array) => {
-    metades = item / 2
-    return metades
+    arrayMetades = item / 2
+    return arrayMetades
 })
 
 console.log(divide2);
